002c11c7259f6d97820ee874df78b5cd2484b761
kenis <1836362346@qq.com>
2025-01-22 13:45:13 +0000

    init repo

---
+++ 两数之和.js
@@ -0,0 +1,18 @@
+function twoSum(nums, target) {
+  const result = []; // 用来存放找到的索引
+  for (let i = 0; i < nums.length; i++) {
+    const needNum = target - nums[i]; // 先算出与当前元素匹配的数字
+    const leftNums = nums.slice(i + 1); // 剩下的数组
+    if (leftNums.includes(needNum)) {
+      result.push(
+        i,
+        leftNums.indexOf(needNum) === 0 ? i + 1 : leftNums.indexOf(needNum) + i,
+      );
+    }
+  }
+  return result;
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
+console.log(twoSum([3, 2, 4], 6)); // [1,2]
+console.log(twoSum([3, 3], 6)); // [0,1]
